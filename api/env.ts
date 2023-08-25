3794e6195f29e1f3971d5911b02318a360c9c813
mythcsj <mythcsj@163.com>
2023-08-25 09:38:46 +0800

    [fix] modify code

--- api/env.ts
+++ api/env.ts
@@ -1,16 +1,22 @@
 import 'dotenv/config';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-const { SECRET_KEY: secretKey, DATABASE_URL: databaseUrl } = process.env;
+const { SECRET_KEY: secretKey, DATABASE_URL: databaseUrl, AUTH_KEY: authKey } = process.env;
 
 export default (request: VercelRequest, response: VercelResponse) => {
-    let error = '';
+    const authToken = request.headers["auth-token"];
+    if (!authToken || authKey != authToken)
+        return response.status(401).send({ message: `please pass in the correct [auth-token]`, data: '' }).end();
+
     const constKey = request.query?.constKey || '';
-    if (!constKey) error = 'parameter [constKey] is empty';
+    if (!constKey)
+        return response.status(400).send({ message: `parameter [constKey] is empty`, data: '' }).end();
+
     const constValue = ENV[`${constKey}`];
-    if (!constValue) error = `the value of parameter [${constKey}] is empty`;
-    if (error) return response.status(400).send({ data: error }).end();
-    response.status(200).send({ data: constValue })
+    if (!constValue)
+        return response.status(400).send({ message: `the value of parameter [${constKey}] is empty`, data: '' }).end();
+
+    response.status(200).send({ message: 'query success', data: constValue })
 }
 
 const ENV = {
